d6f78e6d85bc8f27c77f3d1013282d34171d0233
Seemant Shukla <13106945+sshukla154@users.noreply.github.com>
2020-11-24 07:52:19 +0530

    Validation - Exception Handling

--- src/main/java/frontier/learning/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/frontier/learning/msscbeerservice/web/controller/BeerController.java
@@ -1,13 +1,19 @@
 package frontier.learning.msscbeerservice.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.function.Consumer;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,6 +61,25 @@ public class BeerController {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteBeer(@PathVariable UUID beerId) {
 		beerService.deleteById(beerId);
+	}
+
+	/*
+	 * This ExceptionHandler(), will be called from create/update beer API. When any
+	 * exception is occurred in these API that will be cached by @Valid annotation
+	 * which will internally call this API
+	 */
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<List> validateErrorHandler(ConstraintViolationException e) {
+		List<String> errors = new ArrayList<String>(e.getConstraintViolations().size());
+		Consumer<ConstraintViolation> consumerAction = new Consumer<ConstraintViolation>() {
+
+			@Override
+			public void accept(ConstraintViolation constraintViolation) {
+				errors.add(constraintViolation.getPropertyPath() + ":" + constraintViolation.getMessage());
+			}
+		};
+		e.getConstraintViolations().forEach(consumerAction);
+		return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
 	}
 
 }
