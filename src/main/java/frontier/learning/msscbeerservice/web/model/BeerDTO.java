7543698dbd6f1f9a3a103fecef2f0af5ca06bf60
Seemant Shukla <13106945+sshukla154@users.noreply.github.com>
2020-11-24 08:26:30 +0530

    Validation at DTO level

--- src/main/java/frontier/learning/msscbeerservice/web/model/BeerDTO.java
+++ src/main/java/frontier/learning/msscbeerservice/web/model/BeerDTO.java
@@ -4,6 +4,10 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,14 +19,29 @@ import lombok.NoArgsConstructor;
 @Builder
 public class BeerDTO {
 
+	@Null
 	private UUID id;
+	
+	@Positive
 	private Integer version;
+	
 	private OffsetDateTime createDate;
+	
 	private OffsetDateTime lastModifiedDate;
+	
+	@NotNull
 	private String beerName;
+	
+	@NotNull
 	private BeerStyleEnum beerStyleName;
+	
+	@Positive
 	private Long upc;
+	
+	@Positive
 	private Integer quantityOnHand;
+	
+	@Positive
 	private BigDecimal price;
 
 }
